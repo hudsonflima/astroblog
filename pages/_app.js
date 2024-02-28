b617a796bcde6a88f05b906f745c28e4d75e1dfc
Hudson Lima <hudsonflima@gmail.com>
2024-02-28 13:12:27 -0300

    SpeedInsights

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,14 @@
 // pages/_app.js
 import '../styles/globals.css';
 import '../styles/icons.css';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 function MyApp({ Component, pageProps }) {
 
   return (
     <div className='flex flex-col'>
       <Component {...pageProps} />
+      <SpeedInsights />
     </div>
   );
 }
