ad8dcc2141720a6591a23d2497dfd0b90a0263a4
Hudson Lima <hudsonflima@gmail.com>
2025-02-04 14:11:21 -0300

    Update Artigo03.js

--- components/Artigo03.js
+++ components/Artigo03.js
@@ -6,7 +6,7 @@ const ArticleCard03 = () => {
         <li className="flex items-center">
           <svg className="w-3.5 h-3.5 me-2 text-green-500 dark:text-green-400 flex-shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
             <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
-          </svg>Como a astrologia pode ser uma ferramenta poderosa para o autoconhecimento e desenvolvimento pessoal.</li>
+          </svg>Como a Astrologia pode ser uma ferramenta poderosa para o autoconhecimento e desenvolvimento pessoal.</li>
         <li className="flex items-center">
           <svg className="w-3.5 h-3.5 me-2 text-green-500 dark:text-green-400 flex-shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
             <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
@@ -18,11 +18,11 @@ const ArticleCard03 = () => {
         <li className="flex items-center">
           <svg className="w-3.5 h-3.5 me-2 text-green-500 dark:text-green-400 flex-shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
             <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
-          </svg>Como a astrologia pode influenciar relacionamentos interpessoais e dinâmicas de grupo.</li>
+          </svg>Como a Astrologia pode influenciar relacionamentos interpessoais e dinâmicas de grupo.</li>
         <li className="flex items-center">
           <svg className="w-3.5 h-3.5 me-2 text-green-500 dark:text-green-400 flex-shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
             <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
-          </svg>Discutir compatibilidade entre signos, sinastria (análise de compatibilidade entre mapas astrais) e como entender melhor as necessidades e comportamentos dos outros através da astrologia.</li>
+          </svg>Discutir compatibilidade entre signos, sinastria (análise de compatibilidade entre mapas astrais) e como entender melhor as necessidades e comportamentos dos outros através da Astrologia.</li>
       </ul>
     </div>
   );
